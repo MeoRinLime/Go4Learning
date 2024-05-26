0479693e4aeb0fbd87d206348ecd890eaeebd4a2
lpshwl <14262238+lpshwl@user.noreply.gitee.com>
2024-05-26 15:46:08 +0800

    完成基础的对course，knowledgePoint，websites的增删改查，并写了接口文档

--- back/back/Controllers/CourseController.cs
+++ back/back/Controllers/CourseController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace back.Controllers
 {
-    
+
     [ApiController]
     [Route("course")]
     public class CourseController : ControllerBase
@@ -20,12 +20,11 @@ namespace back.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        
-        public Result<List<Course>> getCourseList()
+        [HttpGet("{id}")]
+        public Result<List<Course>> getCourseList(int id)
         {
 
-            return courseService.getCourseList(); ;
+            return courseService.getCourseList(id);
         }
 
         [HttpPost("addCourse")]
@@ -35,10 +34,51 @@ namespace back.Controllers
         }
 
         [HttpPost("addWebSiteList")]
-        public Result<string> addWebsiteList([FromBody]List<WebSite> webSites)
+        public Result<List<WebSite>> addWebsiteList([FromBody] List<WebSite> webSites)
         {
             return courseService.addWebsiteList(webSites);
         }
+        [HttpPost("addKnowledgePointList")]
+        public Result<List<KnowledgePoint>> addKnowledgePointList(List<KnowledgePoint> knowledgePoints)
+        {
+            return courseService.addKnowledgePointList(knowledgePoints);
+        }
+
+        [HttpPost("updateWebSiteList")]
+
+        public Result<List<WebSite>> updateWebsiteList([FromBody] List<WebSite> webSites)
+        {
+            return courseService.updateWebsiteList(webSites);
+        }
+
+        [HttpPost("updateKnowledgePointList")]
+        public Result<List<KnowledgePoint>> updateKnowledgePointList(List<KnowledgePoint> knowledgePoints)
+        {
+            return courseService.updateKnowledgePointList(knowledgePoints);
+        }
+        [HttpPost("updateCourse")]
+        public Result<Course> removeCourse(Course course)
+        {
+            return courseService.updateCourse(course);
+        }
+
+
+        [HttpPost("removeWebSiteList")]
+        public Result<string> removeWebsiteList([FromBody] List<WebSite> webSites)
+        {
+
+            return courseService.removeWebsiteList(webSites);
+        }
+        [HttpPost("removeKnowledgePointList")]
+        public Result<string> removeKnowledgePointList(List<KnowledgePoint> knowledgePoints)
+        {
+            return courseService.removeKnowledgePointList(knowledgePoints);
+        }
+        [HttpGet("removeCourse/{id}")]
+        public Result<string> removeCourse(int id) {
+            return courseService.removeCourse(id);
+        }
+        
     }
     
 }
--- back/back/Service/CourseService.cs
+++ back/back/Service/CourseService.cs
@@ -14,12 +14,16 @@ namespace back.Service
             this.courseContext = context;
         }
 
-        public Result<List<Course>> getCourseList()
+        public Result<List<Course>> getCourseList(int id)
         {
             try
             {
-                var query = courseContext.course.Where(c=>c.CourseState==1);
-                List<Course> result = query.ToList();
+                List<Course> result;
+                if (id == -1)
+                    result = courseContext.course.Where(c=>c.CourseState==1).ToList();
+                else
+                    result = courseContext.course.Where(c => c.CourseCreatorId==id).ToList();
+                
                 if (result.Count <= 0)
                 {
                     return Result<List<Course>>.error("当前没有课程");
@@ -57,18 +61,132 @@ namespace back.Service
            
         }
 
-        public Result<string> addWebsiteList(List<WebSite> webSites)
+        public Result<List<WebSite> > addWebsiteList(List<WebSite> webSites)
         {
             try
             {
                 courseContext.webSites.AddRange(webSites);
                 courseContext.SaveChanges();
-                return Result<string>.success("正常");
+                return Result< List < WebSite> >.success(webSites);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                return Result<List<WebSite>>.error("服务器出错");
+            }
+        }
+
+        public Result<List<KnowledgePoint>> addKnowledgePointList(List<KnowledgePoint> knowledgePoints)
+        {
+            try
+            {
+                courseContext.knowledgePoints.AddRange(knowledgePoints);
+                courseContext.SaveChanges();
+                return Result<List<KnowledgePoint>>.success(knowledgePoints);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                return Result<List<KnowledgePoint>>.error("服务器出错");
+            }
+        }
+
+        public Result<string> removeCourse(int id)
+        {
+            try
+            {
+                var ks= courseContext.knowledgePoints.Where(k => k.CourseId == id);
+                courseContext.RemoveRange(ks);
+                var ws = courseContext.webSites.Where(w => w.CourseId == id);
+                courseContext.RemoveRange(ws);
+                Course? c= courseContext.course.FirstOrDefault(k=>k.CourseId == id);
+                if(c != null) 
+                courseContext.course.Remove(c);
+                courseContext.SaveChanges();
+                return Result<string>.success("删除成功");
             }
             catch (Exception ex)
             {
                 Console.Write(ex.Message);
                 return Result<string>.error("服务器出错");
+            }
+        }
+
+        public Result<string> removeWebsiteList(List<WebSite> webSites)
+        {
+            try
+            {
+
+                courseContext.RemoveRange(webSites);
+                courseContext.SaveChanges();
+                return Result<string>.success("删除成功");
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                return Result<string>.error("服务器出错");
+            }
+        }
+
+        public Result<string> removeKnowledgePointList(List<KnowledgePoint> knowledgePoints)
+        {
+            try
+            {
+
+                courseContext.RemoveRange(knowledgePoints);
+                courseContext.SaveChanges();
+                return Result<string>.success("删除成功");
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                return Result<string>.error("服务器出错");
+            }
+        }
+
+        public Result<List<WebSite>> updateWebsiteList(List<WebSite> webSites)
+        {
+            try
+            {
+
+                courseContext.UpdateRange(webSites);
+                courseContext.SaveChanges();
+                return Result< List<WebSite>>.success(webSites);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                return Result<List<WebSite>>.error("服务器出错");
+            }
+        }
+
+        public Result<List<KnowledgePoint>> updateKnowledgePointList(List<KnowledgePoint> knowledgePoints)
+        {
+            try
+            {
+                courseContext.UpdateRange(knowledgePoints);
+                courseContext.SaveChanges();
+                return Result<List<KnowledgePoint>>.success(knowledgePoints);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                return Result<List<KnowledgePoint>>.error("服务器出错");
+            }
+        }
+
+        public Result<Course> updateCourse(Course course)
+        {
+            try
+            {
+                courseContext.Update(course);
+                courseContext.SaveChanges();
+                return Result<Course>.success(course);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                return Result<Course>.error("服务器出错");
             }
         }
     }
