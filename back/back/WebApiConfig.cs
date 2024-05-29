d39aa07714dbc58cbe8c9dc290723bb49fc56f6f
lpshwl <14262238+lpshwl@user.noreply.gitee.com>
2024-05-29 16:07:51 +0800

    跨域

---
+++ back/back/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace back
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //跨域配置
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            
+        }
+    }
+
+}
